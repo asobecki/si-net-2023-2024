6850f9fa0ab39e8a3b5d0a9d3f66e0f7c825cd31
asobecki <andrzej.sobecki@pm.me>
2023-10-04 19:15:06 +0200

    Bardzo podstawowa aplikacja

---
+++ w1basic/Program.cs
@@ -0,0 +1,9 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+app.Use(async (context, next) =>
+{
+    context.Response.WriteAsync("testy testy");
+    await next.Invoke();
+});
+app.Run();
+
