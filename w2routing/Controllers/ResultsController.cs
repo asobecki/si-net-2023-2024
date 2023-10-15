3dae4eb168785c4801279d4002f7016d54b7101a
asobecki <andrzej.sobecki@pm.me>
2023-10-15 19:22:48 +0200

    [W2] Przykłady odpowiedzi

---
+++ w2routing/Controllers/ResultsController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace w2routing.Controllers
+{
+    public class ResultsController : Controller
+    {
+
+        // 1. Zwracanie dowolnej klasy - serializowanej
+        //
+        // 2. Zwracanie XML json
+        //
+        // 3. status 400
+        //
+        // 4. status 401
+        //
+        // 5. status 200
+        //
+        // 6. status 201
+        //
+        // 7. status 403
+        //
+        // 8. status 404
+        //
+        // 9. redirect do akcji
+        //
+        // 10. zwracanie widoku częściowego
+
+    }
+}
